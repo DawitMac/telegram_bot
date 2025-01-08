ec48141a09cdc0479b4da8fabf4cab9415228674
DawitMac <tigistugaraneh5@gmail.com>
2025-01-08 13:34:11 +0300

    .gitignore file added

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const { Telegraf } = require('telegraf')
-const dotenv = require('dotenv').config();
-const Token = process.env.TOKEN
-const bot = new Telegraf(process.env.BOT_TOKEN || Token)
-bot.start((ctx) => ctx.reply('Welcome' , { 
-  reply_markup : { 
-    keyboard: [[{text: "web_app" , web_app : {url :"https://hagerecrafts-cnzi.vercel.app/"}}]],
-     },
-    }))
+const { Telegraf } = require('telegraf');
+require('dotenv').config();
+
+const BOT_TOKEN = process.env.TOKEN;
+
+if (!BOT_TOKEN) {
+    console.error('Telegram bot token is missing in environment variables.');
+    process.exit(1);
+}
+
+const bot = new Telegraf(BOT_TOKEN);
+
+bot.start((ctx) => {
+    ctx.reply('Welcome', {
+        reply_markup: {
+            keyboard: [[{ text: "web_app", url: "https://hagerecrafts-cnzi.vercel.app/" }]]
+        }
+    });
+});
+
 bot.launch();
