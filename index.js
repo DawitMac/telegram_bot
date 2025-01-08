6b8166819413ad8a0f0c191409ce1badc39c898b
DawitMac <tigistugaraneh5@gmail.com>
2025-01-08 23:48:38 +0300

    webhook url adjusted well

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-const { Telegraf } = require('telegraf')
-const Token = "8184163034:AAGpG9AQFKtvufsz6YgGSuVwso2ZeaCtaro"
-const bot = new Telegraf(Token)
-bot.start((ctx) => ctx.reply('Welcome' , { 
-  reply_markup : { 
-    keyboard: [[{text: "HagereCrafts" , web_app : {url :"https://hagerecrafts-cnzi.vercel.app/"}}]],
-     },
-    }))
-bot.launch();
+const { Telegraf } = require('telegraf');
+
+module.exports = async (req, res) => {
+    const bot = new Telegraf("8184163034:AAGpG9AQFKtvufsz6YgGSuVwso2ZeaCtaro");
+
+    bot.start((ctx) => {
+        ctx.reply('Welcome', {
+            reply_markup: {
+                keyboard: [[{ text: "HagereCrafts", url: "https://hagerecrafts-cnzi.vercel.app/" }]]
+            }
+        });
+    });
+
+    await bot.handleUpdate(req.body, res);
+};
+
+//
+
+
+// const { Telegraf } = require('telegraf')
+// const Token = "8184163034:AAGpG9AQFKtvufsz6YgGSuVwso2ZeaCtaro"
+// const bot = new Telegraf(Token)
+// bot.start((ctx) => ctx.reply('Welcome' , { 
+//   reply_markup : { 
+//     keyboard: [[{text: "HagereCrafts" , web_app : {url :"https://hagerecrafts-cnzi.vercel.app/"}}]],
+//      },
+//     }))
+// bot.launch();
