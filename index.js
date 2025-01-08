5805d00ebe115039116f41356b25644177718c88
DawitMac <tigistugaraneh5@gmail.com>
2025-01-08 21:22:31 +0300

    telegram token adjusted

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Telegraf } = require('telegraf')
 const Token = "8184163034:AAGpG9AQFKtvufsz6YgGSuVwso2ZeaCtaro"
-const bot = new Telegraf(process.env.BOT_TOKEN || Token)
+const bot = new Telegraf(Token)
 bot.start((ctx) => ctx.reply('Welcome' , { 
   reply_markup : { 
     keyboard: [[{text: "HagereCrafts" , web_app : {url :"https://hagerecrafts-cnzi.vercel.app/"}}]],
