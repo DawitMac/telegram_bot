b3d6028fb869b5b0fcc3e8be501f8ae3f1815e34
DawitMac <tigistugaraneh5@gmail.com>
2025-01-09 09:52:19 +0300

    flag that checks weather the bot is started or not added to index.js

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-// const { Telegraf } = require('telegraf');
 
-// module.exports = async (req, res) => {
-//     const bot = new Telegraf("8184163034:AAGpG9AQFKtvufsz6YgGSuVwso2ZeaCtaro");
+// const { Telegraf } = require('telegraf')
+// // const dotenv = require('dotenv').config();
+// const Token = "8184163034:AAGpG9AQFKtvufsz6YgGSuVwso2ZeaCtaro"
+// const bot = new Telegraf(Token)
+// bot.start((ctx) => ctx.reply('Welcome' , { 
+//   reply_markup : { 
+//     keyboard: [[{text: "web_app" , web_app : {url :"https://hagerecrafts-cnzi.vercel.app/"}}]],
+//      },
+//     }))
+// bot.launch();
 
-//     bot.start((ctx) => {
-//         ctx.reply('Welcome', {
-//             reply_markup: {
-//                 keyboard: [[{ text: "HagereCrafts", url: "https://hagerecrafts-cnzi.vercel.app/" }]]
-//             }
-//         });
-//     });
-
-//     await bot.handleUpdate(req.body, res);
-// };
-
-
-
-
-const { Telegraf } = require('telegraf')
-// const dotenv = require('dotenv').config();
+const { Telegraf } = require('telegraf');
 const Token = "8184163034:AAGpG9AQFKtvufsz6YgGSuVwso2ZeaCtaro"
-const bot = new Telegraf(Token)
-bot.start((ctx) => ctx.reply('Welcome' , { 
-  reply_markup : { 
-    keyboard: [[{text: "web_app" , web_app : {url :"https://hagerecrafts-cnzi.vercel.app/"}}]],
-     },
-    }))
-bot.launch();
+const bot = new Telegraf(Token);
+
+// Flag to track bot state
+let isBotRunning = false;
+
+// Handler for bot start command
+bot.start((ctx) => {
+  if (!isBotRunning) {
+    isBotRunning = true;
+    ctx.reply('Welcome', {
+      reply_markup: {
+        keyboard: [[{ text: "web_app", web_app: { url: "https://hagerecrafts-cnzi.vercel.app/" } }]]
+      }
+    });
+  } else {
+    ctx.reply('Bot is already running.');
+  }
+});
+
+// Launch the bot
+if (!isBotRunning) {
+  bot.launch();
+}
